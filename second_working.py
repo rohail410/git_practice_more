3b0bef4ad6118fb74fb1e4e17ac46914a95e143a
Rohail <rohailtabassumit@gmail.com>
2025-10-23 14:15:08 +0500

    Understanding git diff to understand difference local and remote repo

--- second_working.py
+++ second_working.py
@@ -3,3 +3,5 @@ print("Another file to working for diff")
 print("Adding third line to second file")
 
 print("Made some changes to this file to push my data online.")
+
+print("Made some changes again to understand git diff on local and remote repo")
